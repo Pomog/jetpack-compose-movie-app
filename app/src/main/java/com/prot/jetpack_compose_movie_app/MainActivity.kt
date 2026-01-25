775d3cf5ef0128603301151b5bd7112e91eb1231
Yurii Panasuik <thoryur@gmail.com>
2026-01-25 23:38:56 +0200

    Initial commit

---
+++ app/src/main/java/com/prot/jetpack_compose_movie_app/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.prot.jetpack_compose_movie_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.prot.jetpack_compose_movie_app.ui.theme.JetpackcomposemovieappTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApp {
+                MainContent()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    JetpackcomposemovieappTheme {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text("Movies") },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        titleContentColor = Color.Magenta,
+                    ),
+                )
+            },
+        ) { innerPadding ->
+            Surface(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+            ){
+                content()
+            }
+        }
+    }
+}
+
+
+@Composable
+fun MainContent(){
+    Surface(color = MaterialTheme.colorScheme.background,
+        modifier = Modifier.fillMaxSize()) {
+        Text(text = "Main Content")
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApp {
+        MainContent()
+    }
+}
